5e70dd4237429f83406ea70a595a893d5b500b9d
wd2nf8gqct <committer@noreply.ethi.cz>
2025-03-20 23:27:40 -0400

    Add device sensitivity and update lid_action script

--- hyprland/hypr/.config/hypr/scripts/lid_action.sh
+++ hyprland/hypr/.config/hypr/scripts/lid_action.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+monitorCount=$(hyprctl monitors | grep -c "Monitor")
+internalMonitor="eDP-1"
+
 # Check if more than one monitor is active
-if [ "$(hyprctl monitors | grep -c "Monitor")" -gt 1 ]; then
+if [ "${monitorCount}" -gt 1 ]; then
     # Multiple monitors active, just disable the laptop screen
-    hyprctl keyword monitor "eDP-1, disable"
+    hyprctl keyword monitor "${internalMonitor}, disable"
+elif [ "${monitorCount}" -eq 1 ] && ! hyprctl monitors | grep -q "${internalMonitor}"; then
+     # Only one monitor active and it's not the internal one
+    hyprctl keyword monitor "${internalMonitor}, disable"
 else
     # Only laptop screen active, lock and suspend
     hyprlock
